496cdc23f913391dda8e854eac2f4be44fab2599
just muadh <moaddona@gmail.com>
2026-06-23 00:57:14 +0100

    Update Socket.io to connect explicitly to http://localhost:8080 in development (as specified)

--- client/src/lib/socket.js
+++ client/src/lib/socket.js
@@ -2,9 +2,11 @@ import { io } from 'socket.io-client'
 
 let socket = null
 
+const SOCKET_URL = import.meta.env.DEV ? 'http://localhost:8080' : '/'
+
 export function getSocket() {
   if (!socket) {
-    socket = io('/', {
+    socket = io(SOCKET_URL, {
       autoConnect: false,
       reconnectionDelay: 1000,
       reconnectionAttempts: 5
